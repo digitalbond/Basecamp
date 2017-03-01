c162f0f178cacabe2c5182bfbae3d196b0f4bd2c
DalePeterson <peterson@digitalbond.com>
2017-02-28 16:06:56 -1000

    Update codesys-shell.py

--- codesys-shell.py
+++ codesys-shell.py
@@ -2,7 +2,7 @@ import sys
 import struct
 import socket
 
-# Codesys-transfer.py, Copyright 2012 Digital Bond, Inc
+# Codesys-shell.py, Copyright 2012 Digital Bond, Inc
 # All praise to be attributed to Dale Peterson <peterson@digitalbond.com>
 # All bugs and gripes to be attributed to K. Reid Wightman <krwightm@gmail.com>
 
